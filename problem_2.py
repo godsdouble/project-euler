aab7419d963c85f2cc5b9a3f1ba5a2070408c97a
Gabriel M <mr.maatouk@gmail.com>
2018-05-24 12:50:14 -0400

    PEP8 Errors fixed
    More formatting fixes

--- problem_2.py
+++ problem_2.py
@@ -1,14 +1,16 @@
 def fib(limit):
     a, b = 1, 2
-    fib_seq=[1]
-    while b<=limit:
+    fib_seq = [1]
+    while b <= limit:
         fib_seq.append(b)
-        a, b = b, a+b
+        a, b = b, a + b
     return fib_seq
 
+
 def even_list(lst):
-    even = [i for i in lst if i%2==0]
+    even = [i for i in lst if i % 2 == 0]
     return even
+
 
 def sum_of(lst):
     sum = 0
@@ -16,7 +18,8 @@ def sum_of(lst):
         sum += x
     return sum
 
+
 fib_sequence = fib(4000000)
 fib_even = even_list(fib_sequence)
 sum_is = sum_of(fib_even)
-print (sum_is)
+print(sum_is)
