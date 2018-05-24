7666499438a36db834e65a0a90647ff8442e3353
Gabriel M <mr.maatouk@gmail.com>
2018-05-24 17:53:50 -0400

    Simplified code

--- problem_5.py
+++ problem_5.py
@@ -1,20 +1,25 @@
 # We know answer will be a multiple of 2520.
-# We need to figure out with numbers from 11 to 20 produce a remainder
-j=2520
-num_check=[]
+# We need to figure out which numbers from 11 to 20 produce a remainder
+num = 2520
+factors = [num]
 
-for i in range(11,21):
-    if j%i!=0:
-        num_check.append(i)
+for i in range(11, 21):
+    if num % i != 0:
+        factors.append(i)
 
-print (num_check)
+while True:
 
-def division1to20 (num):
-    if num%11==0 and num%13==0 and num%16==0 and num%17==0 and num%19==0:
-        return False
-    else:
-        return True
+    mod_num = [num % factor for factor in factors]
 
-while division1to20(j):
-    j+=2520
-print (j)
+    # if list has a mod = 0 for each factor in this list, then set size
+    # will be 1 with value 0 inside. I do not need to make a check for 0
+    # because first factor is 2520 and it is being incremented by 2520,
+    # so this mod will always be 0.
+
+    if len(set(mod_num)) == 1:
+        break
+    num += 2520
+
+print(num)
+
+# outputs 232792560
