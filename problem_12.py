af08095752e98358bceba33f27ddce71923e0a31
Gabriel M <mr.maatouk@gmail.com>
2018-05-24 23:13:28 -0400

    PEP8 error fixes

--- problem_12.py
+++ problem_12.py
@@ -1,25 +1,32 @@
 import time
 start_time = time.time()
 
-def how_many(num):    
-    result = set() # using a set removes any duplicate factors
-    for i in range(1, int(num ** 0.5) + 1): # only need to check until sqrt of number
-        div, mod = divmod(num, i) # great function which returns quotient AND remainder
+
+def how_many(num):
+    result = set()  # using a set removes any duplicate factors
+    for i in range(1, int(num ** 0.5) + 1):  # only need to check until
+    # sqrt of number
+        # great function which returns quotient AND remainder
+        div, mod = divmod(num, i)
         if mod == 0:
-            result |= {i, div} # new set with elements from both s and t
+            result |= {i, div}  # return set result with elements added
     return len(result)
 
+
 def triangle_num(j):
-    sum=0
+    sum = 0
     while True:
-        sum+=j
+        sum += j
         yield sum
-        j+=1
+        j += 1
+
 
 for el in triangle_num(1):
-    if how_many(el)>500:
-        print (el, how_many(el))
-        print ("Done.")
+    if how_many(el) > 500:
+        print(el, how_many(el))
+        print("Done.")
         break
 
-print("--- %s seconds ---" % (time.time() - start_time))
+print(f"--- {time.time() - start_time} seconds ---"
+
+# outputs 76576500
