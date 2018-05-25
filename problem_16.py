f3c2b93b56f18a5665e8f31bc3dfd0185a127457
Gabriel M <mr.maatouk@gmail.com>
2018-05-25 10:13:41 -0400

    PEP8 errors fixed

--- problem_16.py
+++ problem_16.py
@@ -1,5 +1,7 @@
-print (sum(list(map(int, list(str(2**1000))))))
+print(sum(list(map(int, list(str(2**1000))))))
 
 # orrrr
 
-print (sum(int(x) for x in str(2**1000)))
+print(sum(int(x) for x in str(2**1000)))
+
+# outputs 1366
