8785a857b632242039d927ee2a3d2b87b53040e2
Gabriel M <mr.maatouk@gmail.com>
2018-05-25 00:04:10 -0400

    Solved new problems

--- problem_14.py
+++ problem_14.py
@@ -1,6 +1,22 @@
 def collatz(n):
-    seq=set(n)
-    even, odd = lambda n, n/2, lambda n, 3*n+1
+    seq.append(n)
+    if n == 1:
+        return 1
+    elif n % 2 == 0:
+        collatz(n / 2)
+    else:
+        collatz(3 * n + 1)
 
-    while num>1:
-        x
+
+longest_chain = 0
+
+for i in range(500001, 1000000, 2):
+    seq = []
+    collatz(i)
+    if len(seq) > longest_chain:
+        longest_chain = len(seq)
+        biggest = i
+
+print(biggest, ":", longest_chain)
+
+# outputs 837799
